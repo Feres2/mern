611bec18078991d7ebb11466c2096a8d2c4e1b28
Feres2 <feresalyani@gmail.com>
2023-03-07 13:09:40 +0100

    feres

--- superNinja.js
+++ superNinja.js
@@ -23,3 +23,21 @@ const ninja1 = new Ninja("Hyabusa");
         ninja1.sayName();
         ninja1.drinkSake();
         ninja1.showStats();
+class Sensei extends Ninja{
+    constructor(name){
+        super(name);
+        this.health =200;
+        this.speed = 10;
+        this.strength = 10;
+        this.wisdom=10;
+    }
+    speakWisdom(){
+        this.drinkSake()
+        console.log("What one programmer can do in one month, two programmers can do in two months.");
+    }
+    
+
+}
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+superSensei.showStats();
